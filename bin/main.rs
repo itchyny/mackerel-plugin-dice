ab8a1ab4569ea7b666e90a622f71cd355abccc9b
itchyny <itchyny@cybozu.co.jp>
2023-11-19 22:47:17 +0900

    update to 2021 edition and update dependencies

--- bin/main.rs
+++ bin/main.rs
@@ -1,16 +1,9 @@
-extern crate mackerel_plugin;
-extern crate mackerel_plugin_dice;
-
 use mackerel_plugin::Plugin;
 use mackerel_plugin_dice::DicePlugin;
 
 fn main() {
-    let plugin = DicePlugin {};
-    match plugin.run() {
-        Ok(_) => {}
-        Err(err) => {
-            eprintln!("mackerel-plugin-dice: {}", err);
-            std::process::exit(1);
-        }
+    if let Err(err) = (DicePlugin {}).run() {
+        eprintln!("mackerel-plugin-dice: {}", err);
+        std::process::exit(1);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,28 @@
-extern crate mackerel_plugin;
-extern crate rand;
-
-use std::collections::HashMap;
 use mackerel_plugin::*;
+use std::collections::HashMap;
 
 pub struct DicePlugin {}
 
 impl Plugin for DicePlugin {
     fn fetch_metrics(&self) -> Result<HashMap<String, f64>, String> {
-        let mut metrics = HashMap::new();
-        metrics.insert(
-            "dice.d6".to_string(),
-            (rand::random::<u64>() % 6 + 1) as f64,
-        );
-        metrics.insert(
-            "dice.d20".to_string(),
-            (rand::random::<u64>() % 20 + 1) as f64,
-        );
-        Ok(metrics)
+        Ok(HashMap::from([
+            ("dice.d6".to_owned(), (rand::random::<u64>() % 6 + 1) as f64),
+            (
+                "dice.d20".to_owned(),
+                (rand::random::<u64>() % 20 + 1) as f64,
+            ),
+        ]))
     }
 
     fn graph_definition(&self) -> Vec<Graph> {
-        vec![
-            graph! {
-                name: "dice",
-                label: "My Dice",
-                unit: "integer",
-                metrics: [
-                    { name: "d6", label: "Die 6" },
-                    { name: "d20", label: "Die 20" },
-                ]
-            },
-        ]
+        vec![graph! {
+            name: "dice",
+            label: "My Dice",
+            unit: "integer",
+            metrics: [
+                { name: "d6", label: "Die 6" },
+                { name: "d20", label: "Die 20" },
+            ],
+        }]
     }
 }
